f8e604be42ebe3c81bd105a5636ff2b7f030f017
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-06-17 13:44:47 -0400

    completed

--- NorthwindTradersSpringBoot/NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -16,36 +16,5 @@ public class NorthwindTradersSpringBootApplication {
 
 		ApplicationContext context = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 		ProductDao productDao = context.getBean(ProductDao.class);
-
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.println("1. List Products");
-			System.out.println("2. Add Products");
-			System.out.println("0. Exit");
-			System.out.print("Choose one of the following.. ");
-			int choice = scanner.nextInt();
-
-			if (choice == 1) {
-				for (Product product : productDao.getAllProducts()) {
-					System.out.println(product);
-				}
-			} else if (choice == 2) {
-				System.out.println("com.pluralsight.NorthwindTradersSpringBoot.models.Product Id ");
-				int productId = scanner.nextInt();
-				scanner.nextLine();
-				System.out.println("com.pluralsight.NorthwindTradersSpringBoot.models.Product Name ");
-				String productName = scanner.nextLine();
-				System.out.println("Category ");
-				String category = scanner.nextLine();
-				System.out.println("Price ");
-				double price = scanner.nextDouble();
-				scanner.nextLine();
-				productDao.addProduct(new Product(productId, productName, category, price));
-			} else if (choice == 0) {
-				break;
-			}
-		}
-
 	}
 }
-
---
+++ NorthwindTradersSpringBoot/NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/ui/App.java
@@ -0,0 +1,48 @@
+package com.pluralsight.NorthwindTradersSpringBoot.ui;
+
+import com.pluralsight.NorthwindTradersSpringBoot.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Scanner;
+
+public class App implements CommandLineRunner {
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public void run(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. List Products");
+            System.out.println("2. Add Products");
+            System.out.println("0. Exit");
+            System.out.print("Choose one of the following.. ");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                for (Product product : productDao.getAllProducts()) {
+                    System.out.println(product);
+                }
+            } else if (choice == 2) {
+                System.out.println("com.pluralsight.NorthwindTradersSpringBoot.models.Product Id ");
+                int productId = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("com.pluralsight.NorthwindTradersSpringBoot.models.Product Name ");
+                String productName = scanner.nextLine();
+                System.out.println("Category ");
+                String category = scanner.nextLine();
+                System.out.println("Price ");
+                double price = scanner.nextDouble();
+                scanner.nextLine();
+                productDao.addProduct(new Product(productId, productName, category, price));
+            } else if (choice == 0) {
+                break;
+            }
+        }
+
+    }
+}
+
+
+
